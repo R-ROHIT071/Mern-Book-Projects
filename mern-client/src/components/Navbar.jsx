606c3dd706c0f512d5fdd289abf867a85e59a902
R_ROHIT071 <rohit07cipherwiz@gmail.com>
2024-04-01 11:34:39 +0530

    first commit

---
+++ mern-client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [isMenuOpen,setMenuOpen]= useState(false);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
